ab3ee344e6c415a6b572b0069d89f4cf57dd8c63
kyu <kyuli94p@gmail.com>
2024-10-23 16:32:34 +0900

    update: metadata 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import React from 'react';
+
+export const metadata: Metadata = {
+  title: '마이 페이지',
+  description: '마이 페이지입니다.'
+};
 
 const page = () => {
   return <div>mypage</div>;
