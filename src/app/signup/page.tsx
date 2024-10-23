d7900a127fcb3bce4916f3c5da7c3b5063e785e3
kyu <kyuli94p@gmail.com>
2024-10-23 16:35:38 +0900

    add: init

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  title: '회원가입 페이지',
+  description: '회원가입 페이지입니다.'
+};
+
+const page = () => {
+  return <div>회원가입 페이지</div>;
+};
+
+export default page;
