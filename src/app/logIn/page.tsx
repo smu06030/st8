77b6614417aada538162ccc2ddb2cced4b1de6a3
kyu <kyuli94p@gmail.com>
2024-10-23 16:40:59 +0900

    add: init

---
+++ src/app/logIn/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  title: '로그인 페이지',
+  description: '로그인 페이지입니다.'
+};
+
+const page = () => {
+  return <div>로그인 페이지</div>;
+};
+
+export default page;
