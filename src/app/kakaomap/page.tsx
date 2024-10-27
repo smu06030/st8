9fa3c44355d20e8967056dd319557a591931ea47
incheol <smu06030>
2024-10-27 23:37:11 +0900

    [#19] feat: 가로스크롤 버튼 기능 구현

--- src/app/kakaomap/page.tsx
+++ src/app/kakaomap/page.tsx
@@ -16,6 +16,8 @@ const KakaoMapPage = () => {
 
   // 선택된 경로 상태 추가
   const [selectedPath, setSelectedPath] = useState<PathType>([]);
+  // 선택된 슬라이드 index
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const { geoList, setGeoList } = useGeoData();
 
@@ -27,41 +29,44 @@ const KakaoMapPage = () => {
     [setGeoList]
   );
 
-  const handlePolygonPath = (path: PathType) => {
+  const handlePolygonPath = (path: PathType, index: number) => {
+    setActiveIndex(index); // 클릭한 폴리곤 index 저장
     setSelectedPath(path); // 클릭한 폴리곤의 path를 상태에 저장
   };
 
   return (
-    <Map
-      id="map"
-      center={location.center}
-      isPanto={location.isPanto}
-      className="relative h-[100vh] w-[100vw]"
-      level={12}
-    >
-      {geoList.map((item, index) => {
-        const { key, path, isHover } = item;
-        const color = MAP_COLOR[index];
+    <>
+      <Map
+        id="map"
+        center={location.center}
+        isPanto={location.isPanto}
+        className="relative h-[100vh] w-[100vw]"
+        level={12}
+      >
+        {geoList.map((item, index) => {
+          const { key, path, isHover } = item;
+          const color = MAP_COLOR[index];
 
-        return (
-          <Polygon
-            key={key}
-            path={path}
-            strokeWeight={2} // 선 두께
-            strokeColor={color} // 선 색깔
-            strokeOpacity={0.8} // 선 불투명도
-            strokeStyle={'solid'} // 선 스타일
-            fillColor={isHover ? color : '#ffffff'} // 채우기 색깔
-            fillOpacity={0.8} // 채우기 불투명도
-            onMouseover={() => updateHoverState(key, true)}
-            onMouseout={() => updateHoverState(key, false)}
-            onClick={() => handlePolygonPath(path)}
-          />
-        );
-      })}
-      <ScrollButtonSwiper />
-      <ReSetttingMapBounds paths={selectedPath} />
-    </Map>
+          return (
+            <Polygon
+              key={key}
+              path={path}
+              strokeWeight={2} // 선 두께
+              strokeColor={color} // 선 색깔
+              strokeOpacity={0.8} // 선 불투명도
+              strokeStyle={'solid'} // 선 스타일
+              fillColor={isHover ? color : '#ffffff'} // 채우기 색깔
+              fillOpacity={0.8} // 채우기 불투명도
+              onMouseover={() => updateHoverState(key, true)}
+              onMouseout={() => updateHoverState(key, false)}
+              onClick={() => handlePolygonPath(path, index)}
+            />
+          );
+        })}
+        <ReSetttingMapBounds paths={selectedPath} />
+      </Map>
+      <ScrollButtonSwiper activeIndex={activeIndex} setActiveIndex={setActiveIndex} setSelectedPath={setSelectedPath} />
+    </>
   );
 };
 export default KakaoMapPage;
--- src/components/kakaomap/ScrollButtonSwiper.tsx
+++ src/components/kakaomap/ScrollButtonSwiper.tsx
@@ -1,32 +1,58 @@
 'use client';
-
-import { Pagination } from 'swiper/modules';
+import React, { useEffect, useRef, useState } from 'react';
+import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import '@/styles/swiper.css';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import useGeoData from '@/hooks/useGeoData';
+import { PathType } from '@/types/kakaomap/CoordRegionCode.type';
 
-const ScrollButtonSwiper = () => {
-  const { geoList } = useGeoData();
+interface ScrollButtonSwiperPropsType {
+  activeIndex: number;
+  setActiveIndex: (activeIndex: number) => void;
+  setSelectedPath: (path: PathType) => void;
+}
+
+const ScrollButtonSwiper = ({ activeIndex, setActiveIndex, setSelectedPath }: ScrollButtonSwiperPropsType) => {
+  const { siDoName } = useGeoData();
+  const swiperRef = useRef<any>(null); // Swiper 인스턴스 참조
+
+  useEffect(() => {
+    if (swiperRef.current) {
+      swiperRef.current.slideTo(activeIndex); // activeIndex 변경 시 slide 이동
+    }
+  }, [activeIndex]);
+
+  // console.log(siDoName);
   return (
-    <Swiper
-      slidesPerView={'auto'}
-      // spaceBetween={30}
-      centeredSlides={true}
-      pagination={{
-        dynamicBullets: true
-        // clickable: true,
-      }}
-      modules={[Pagination]}
-      className="mySwiper mb-8 mt-8"
-    >
-      {geoList.map((geo) => (
-        <SwiperSlide key={geo.name} className="m-4 p-4 text-center">
-          {geo.name}
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="fixed bottom-0 left-2/4 z-[99] w-[100vw] -translate-x-1/2 transform bg-gradient-to-t from-[rgba(66,66,66,0.8)] to-[rgba(255,255,255,0)]">
+      <Swiper
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
+        slidesPerView={5}
+        spaceBetween={30}
+        centeredSlides={true}
+        grabCursor={true}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true
+        }}
+        navigation={true}
+        onSlideChange={(swiper) => {
+          setActiveIndex(swiper.activeIndex);
+          setSelectedPath(siDoName[swiper.activeIndex].path);
+        }}
+        modules={[Navigation, Pagination]}
+        className="swiper"
+      >
+        {siDoName.map((sido, index) => (
+          <SwiperSlide key={sido.key}>
+            <div className={`swiper-slide ${activeIndex === index ? 'swiper-slide-active' : ''}`}>{sido.name}</div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
