1ef424162363e0c3e874b12864ac4c672d1efb43
incheol <smu06030>
2024-10-28 21:25:37 +0900

    [#22] refactor: siDoFormatter.ts formattedName 메서드 분리

--- src/utils/region/siDoFormatter.ts
+++ src/utils/region/siDoFormatter.ts
@@ -3,10 +3,12 @@ import { GeoData } from '@/types/kakaomap/CoordRegionCode.type';
 // 시도 이름 포멧
 export const siDoFormatter = (geoList: GeoData[]) => {
   return geoList.map((geo) => {
-    const formattedName = geo.name
-      .replace(/특별시|광역시|특별자치도|특별자치시|도/g, '') // '특별시', '광역시', '특별자치도', '특별자치시', '도' 제거
-      .replace(/충청|전라|경상/, (match) => match.slice(0, 1)); // '충청북도' -> '충북', '전라남도' -> '전남', '경상남도' -> '경남'
-
-    return { ...geo, name: formattedName };
+    ({ ...geo, name: formattedName(geo) });
   });
+};
+
+export const formattedName = (geo: GeoData) => {
+  return geo.name
+    .replace(/특별시|광역시|특별자치도|특별자치시|도/g, '') // '특별시', '광역시', '특별자치도', '특별자치시', '도' 제거
+    .replace(/충청|전라|경상/, (match) => match.slice(0, 1)); // '충청북도' -> '충북', '전라남도' -> '전남', '경상남도' -> '경남'
 };
