498304dcc8b133ff5082d6460552461d3951bd8e
임보라 <imbora@imbolaui-MacBookAir.local>
2024-10-31 21:19:24 +0900

    [#28]style:css일부작업

--- src/components/photoalbum/AlbumList.tsx
+++ src/components/photoalbum/AlbumList.tsx
@@ -1,27 +1,36 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useMutation, useQueryClient, useQuery } from '@tanstack/react-query';
-import AddPhotoBtn from './AddPhotoBtn';
 import { fetchAlbum, addAlbumList, deleteAlbumList } from '@/apis/fetchAlbumList';
-import ImgModal from './ImgModal';
+import ImgModal from '@/components/photoalbum/ImgModal';
+import AddPhotoBtn from '@/components/photoalbum/AddPhotoBtn';
 
 const AlbumList = () => {
   const queryClient = useQueryClient();
-  const [imgSrc, setImgSrc] = useState<string[]>([]); //이미지url
-  const [activeTab, setActiveTab] = useState('allTab'); //탭상태
+  const [imgSrc, setImgSrc] = useState<string[]>([]);
+  const [activeTab, setActiveTab] = useState('allTab');
   const [imgModal, setImgModal] = useState(false);
   const [selectedImgUrl, setSelectedImgUrl] = useState('');
   const [edit, setEdit] = useState(false);
   const [deleteId, setDeleteId] = useState<number[]>([]);
 
+  //편집안할땐 체크 다 풀기
+  useEffect(() => {
+    if (!edit) {
+      setDeleteId([]);
+    }
+  }, [edit]);
+
+  //이미지 클릭시 이미지 모달이벤트
   const onClickImgModal = (url: string) => {
     setSelectedImgUrl(url);
     setImgModal(true);
   };
 
-  //탭엑션
+  //탭 액션
   const onClickTab = (tab: string) => {
     setActiveTab(tab);
   };
@@ -31,7 +40,6 @@ const AlbumList = () => {
     mutationFn: addAlbumList,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['photo'] });
-      // queryClient.refetchQueries(['photo']);
     },
     onError: (error) => {
       console.error('MutationError:', error);
@@ -49,6 +57,7 @@ const AlbumList = () => {
     }
   });
 
+  //체크이벤트로 아이디값 배열로 담기
   const handleCheckboxChange = (id: number) => {
     setDeleteId((prev) => {
       if (prev.includes(id)) {
@@ -60,6 +69,7 @@ const AlbumList = () => {
     });
   };
 
+  //선택한이미지 삭제이벤트(유효성검사)
   const onHandleDelete = async () => {
     if (deleteId.length === 0) {
       alert('선택된 앨범이 없습니다.');
@@ -70,7 +80,7 @@ const AlbumList = () => {
     }
   };
 
-  //useQuery : 앨범전체테이블 = albumListData
+  //useQuery (앨범전체테이블 = albumListData)
   const {
     data: albumListData,
     isPending,
@@ -78,9 +88,8 @@ const AlbumList = () => {
   } = useQuery({
     queryKey: ['photo'],
     queryFn: fetchAlbum
-    // refetchOnWindowFocus: true
   });
-  //   console.log('albumListData', albumListData);
+
   if (isPending) return <div>Loading...</div>;
   if (isError) return <div>Error loading data</div>;
 
@@ -93,34 +102,43 @@ const AlbumList = () => {
 
   return (
     <div>
-      <h2 className="m-[24px] border-b border-black pb-[10px] font-bold text-[24px]">나의 추억들</h2>
+      <h2 className="mx-[24px] mt-[38px] border-b border-[#9C9C9C] py-[14px] font-black text-[24px] text-[#004157]">
+        나의 추억들
+      </h2>
 
       {/* 전체보기-지역별 탭버튼 */}
-      <ul className="mx-[24px] flex gap-[10px]">
-        <li
-          className={`albumTab cursor-pointer ${activeTab === 'allTab' ? 'active' : ''}`}
-          onClick={() => onClickTab('allTab')}
-        >
-          전체보기
-        </li>
-        <li
-          className={`albumTab cursor-pointer ${activeTab === 'rigionTab' ? 'active' : ''}`}
-          onClick={() => onClickTab('rigionTab')}
-        >
-          지역별
-        </li>
-        <button className={`text-${edit ? 'red-500' : 'black'}`} onClick={() => setEdit((prev) => !prev)}>
-          편집
-        </button>
-        {edit && (
-          <button className={`text-${edit ? 'red-500' : 'black'}`} onClick={onHandleDelete}>
-            삭제
+      <ul className="mx-[24px] flex justify-between">
+        <div className="flex">
+          <li
+            className={`albumTab cursor-pointer px-[12px] py-[18px] ${activeTab === 'allTab' ? 'active' : ''}`}
+            onClick={() => onClickTab('allTab')}
+          >
+            전체보기
+          </li>
+          <li
+            className={`albumTab cursor-pointer px-[12px] py-[18px] ${activeTab === 'rigionTab' ? 'active' : ''}`}
+            onClick={() => onClickTab('rigionTab')}
+          >
+            지역별
+          </li>
+        </div>
+        <div>
+          <button
+            className={`text-${edit ? 'red-500' : 'black'} px-[12px] py-[18px]`}
+            onClick={() => setEdit((prev) => !prev)}
+          >
+            편집
           </button>
-        )}
+          {edit && (
+            <button className={`text-${edit ? 'red-500' : 'black'} px-[12px] py-[18px]`} onClick={onHandleDelete}>
+              삭제
+            </button>
+          )}
+        </div>
       </ul>
       {/* 전체보기 */}
       {activeTab === 'allTab' ? (
-        <ul className="mt-4 grid grid-cols-3 gap-[5px]">
+        <ul className="mt-[16px] grid grid-cols-3 gap-[6px]">
           <AddPhotoBtn
             imgSrc={imgSrc}
             setImgSrc={setImgSrc}
@@ -128,42 +146,44 @@ const AlbumList = () => {
             activeTab={activeTab}
             item=""
           />
-          {albumListData?.map((item, index) => (
-            <li key={item.id} className="h-[200px] overflow-hidden border border-black">
+          {albumListData?.map((item) => (
+            <li
+              key={item.id}
+              className={`${edit && deleteId.includes(item.id) && 'border-red-500'} relative aspect-square overflow-hidden border`}
+            >
               {item.photoImg && (
-                <div>
+                <>
                   <Image
                     onClick={() => onClickImgModal(item.photoImg)}
                     src={item.photoImg}
                     alt=""
                     width={200}
-                    height={150}
+                    height={200}
                     priority
-                    className="h-full"
+                    className="h-full w-full object-cover"
                   />
                   {edit && (
                     <input
                       type="checkbox"
-                      className="mr-2"
+                      className="absolute right-[10px] top-[10px] h-6 w-6 appearance-none rounded-full border border-gray-300 text-red-500 checked:border-red-500 checked:bg-[red]"
                       checked={deleteId.includes(item.id)} //배열에 들은 아이디가 있어? 트루펄스
                       onChange={() => handleCheckboxChange(item.id)}
                     />
                   )}
-                </div>
+                </>
               )}
             </li>
           ))}
         </ul>
       ) : (
-        <section className="m-[24px] flex flex-col">
-          <div className="flex flex-col gap-[50px]">
+        <section className="mx-[24px] my-[18px] flex flex-col">
+          <div className="flex flex-col gap-[34px]">
             {filterRigionTitle.map((item, index) => (
               <div key={item}>
-                <div className="flex items-center border-b border-black">
-                  <h2 className="pb-[10px] font-bold text-[24px]">{item}</h2>
-                  <span>{filterRigionPhoto[index]?.length}</span>
+                <div className="flex items-center border-b border-[#9C9C9C]">
+                  <h2 className="py-[14px] font-bold text-[24px]">{item}</h2>
                 </div>
-                <ul className="mt-4 grid grid-cols-3 gap-[5px]">
+                <ul className="mt-[16px] grid grid-cols-3 gap-[6px]">
                   <AddPhotoBtn
                     imgSrc={imgSrc}
                     setImgSrc={setImgSrc}
@@ -171,33 +191,40 @@ const AlbumList = () => {
                     activeTab={activeTab}
                     item={item}
                   />
+                  {/* <Link href={`${NAV_BASE_URL}/setting`}>
+                  <li></li>
+                  </Link> */}
+
                   {filterRigionPhoto[index]?.map((item) => (
-                    <li key={item.id} className="h-[200px] overflow-hidden border border-black">
+                    <li
+                      key={item.id}
+                      className={`${deleteId.includes(item.id) && 'border-red-500'} relative aspect-square overflow-hidden border`}
+                    >
                       {item.photoImg && (
-                        <div>
+                        <>
                           <Image
                             onClick={() => onClickImgModal(item.photoImg)}
                             src={item.photoImg}
                             alt=""
                             width={200}
-                            height={150}
+                            height={200}
                             priority
-                            className="h-full"
+                            className="h-full w-full object-cover"
                           />
                           {edit && (
                             <input
                               type="checkbox"
-                              className="mr-2"
+                              className="absolute right-[10px] top-[10px] h-6 w-6 appearance-none rounded-full border border-gray-300 text-red-500 checked:border-red-500 checked:bg-[red]"
                               checked={deleteId.includes(item.id)} //배열에 들은 아이디가 있어?
                               onChange={() => handleCheckboxChange(item.id)}
                             />
                           )}
-                          {/* {imgModal && <ImgModal setImgModal={setImgModal} imgUrl={item.photoImg} />} */}
-                        </div>
+                        </>
                       )}
                     </li>
                   ))}
                 </ul>
+                <span>{filterRigionPhoto[index]?.length}장</span>
               </div>
             ))}
           </div>
@@ -209,3 +236,7 @@ const AlbumList = () => {
 };
 
 export default AlbumList;
+
+{
+  /* <Link href={`${NAV_BASE_URL}/setting`}></Link> */
+}
