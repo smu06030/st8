335a4cf72e7e66588500529333d255e6b3befa8d
incheol <smu06030>
2024-11-01 18:13:42 +0900

    [#39] fix: 로딩 로직 수정

--- src/app/stamp-map/loading.tsx
+++ src/app/stamp-map/loading.tsx
@@ -1,12 +1,7 @@
 import LoadingSpin from '@/components/common/Loading/Loading';
 
 const Loading = () => {
-  return (
-    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-backgroundGradient">
-      <LoadingSpin />
-      <div className="text-lg mt-4 leading-[27px] text-secondary-900">모아가 여행을 준비중이에요</div>
-    </div>
-  );
+  return <LoadingSpin />;
 };
 
 export default Loading;
