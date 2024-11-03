ba55d5987fe859b92ac7164c12caf075a20cef3f
임보라 <imbora@imbolaui-MacBookAir.local>
2024-11-04 00:33:05 +0900

    [#8]feat:스탬프  목록 비활성화 스탬프 리스트 추가

--- .history/src/components/stamp/StampImg_20241027220527.ts
+++
@@ -1,8 +0,0 @@
-export const STAMPIMG_REGION_NAME: { [key: string]: string } = {
-  //TODO :이미지명이랑 키값 동일하게하기
-  서울: '/images/서울.png',
-  인천: '/images/인천.png',
-  전북특별자치도: '/images/전북특별자치도.png',
-  대구: '/images/대구.png',
-  부산: '/images/부산.png'
-};
