92cdb12138a02237118d9c06b43fa96410eb3648
임보라 <imbora@imbolaui-MacBookAir.local>
2024-11-05 21:21:16 +0900

    [#60]feat:관련링크들 같이 묶는중

--- src/hooks/useNavActive.ts
+++ src/hooks/useNavActive.ts
@@ -3,11 +3,13 @@ import { useState, useEffect } from 'react';
 
 export const useNavActive = () => {
   const params = useParams();
+
   const [activePage, setActivePage] = useState('/');
+  const currentPath = window.location.pathname;
 
   useEffect(() => {
-    const currentPath = window.location.pathname;
     setActivePage(currentPath);
+    console.log('currentPath', currentPath);
   }, [params]);
 
   const handleClick = (link: string) => {
@@ -22,3 +24,19 @@ export const useNavActive = () => {
 };
 
 export default useNavActive;
+
+// if (currentPath.startsWith('/stamp-all')) {
+//   setActivePage('/stamp-all');
+// } else if (currentPath.startsWith('/photo-album')) {
+//   setActivePage('/photo-album');
+// } else if (currentPath.startsWith('/tourism')) {
+//   setActivePage('/tourism');
+// } else if (currentPath.startsWith('/mypage')) {
+//   setActivePage('/tourism');
+// } else if (currentPath === '/') {
+//   setActivePage('/');
+// } else if (currentPath.startsWith('/stamp-tracking')) {
+//   setActivePage('/stamp-all');
+// } else {
+//   setActivePage(''); // 정의되지 않은 페이지는 빈 제목
+// }
