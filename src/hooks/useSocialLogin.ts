30e16e03c1d79a6c081e089f10cdfede744b8b92
kyu <kyuli94p@gmail.com>
2024-11-06 12:33:30 +0900

    [#44] update: dev pull

--- src/hooks/useSocialLogin.ts
+++ src/hooks/useSocialLogin.ts
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/navigation';
-import supabase from '@/utils/supabase/client';
+// import supabase from '@/utils/supabase/client';
+import browserClient from '@/utils/supabase/client';
 
 const getRedirectUrl = () => {
   return process.env.NODE_ENV === 'development'
@@ -11,7 +12,7 @@ export const useSocialLogin = () => {
   const router = useRouter();
 
   const loginWithProvider = async (provider: 'kakao' | 'google') => {
-    const { data, error } = await supabase.auth.signInWithOAuth({
+    const { data, error } = await browserClient.auth.signInWithOAuth({
       provider,
       options: {
         redirectTo: getRedirectUrl()
