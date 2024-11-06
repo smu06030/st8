2fbaa2af8208f566e588ce078071bcd9db877b90
kyu <kyuli94p@gmail.com>
2024-11-06 10:03:19 +0900

    [#44] refactor: pagename 상수화및 header layout 폴더로 이동

---
+++ src/constants/pageName.ts
@@ -0,0 +1,25 @@
+export const PAGE_NAMES = {
+  HOME: { page: '홈', link: '/' },
+  MAP: { page: '지도', link: '/stamp-map' },
+  ALBUM: { page: '앨범', link: '/photo-album' },
+  STAMP: { page: '스탬프', link: '/stamp-all' },
+  STAMP_TRACKING: { page: '스탬프 찍기', link: '/stamp-tracking' },
+  TOURISM: { page: '추천 여행지', link: '/tourism' },
+  MYPAGE: { page: '마이페이지', link: '/mypage' },
+  LOGIN: { page: '로그인', link: '/login' },
+  SIGNUP: { page: '회원가입', link: '/signup' },
+  FORGOT_PASSWORD: { page: '비밀번호 찾기', link: '/forgot-password' }
+};
+
+export const PAGE_PATHS = {
+  HOME: '/',
+  STAMP_ALL: '/stamp-all',
+  PHOTO_ALBUM: '/photo-album',
+  TOURISM: '/tourism',
+  MY_PAGE: '/mypage',
+  LOGIN: '/login',
+  SIGN_UP: '/signup',
+  FORGOT_PASSWORD: '/forgot-password',
+  STAMP_MAP: '/stamp-map',
+  STAMP_TRACKING: '/stamp-tracking'
+};
