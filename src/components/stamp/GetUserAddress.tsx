b1f1e916f008250b93afe388b9ba55c106ad8ba9
lim-bora <lbrkr96@naver.com>
2024-11-13 21:24:19 +0900

    [#128]add:위치권한검사시유효성추가

--- src/components/stamp/GetUserAddress.tsx
+++ src/components/stamp/GetUserAddress.tsx
@@ -39,27 +39,43 @@ const GetUserAddress = () => {
 
   // Geolocation API 로 유저의 위도,경도값 추출
   useEffect(() => {
-    if ('geolocation' in navigator) {
-      //현 브라우저가 Geolocation API를 지원하는지 확인
-      navigator.geolocation.getCurrentPosition(
-        //사용자의 현재 위치를 요청
-        async (position) => {
-          const { latitude, longitude } = position.coords;
-          setLocation({ lat: latitude, lng: longitude });
-          await getAddress(latitude, longitude);
-        },
-        (err) => {
-          showErrorMsg(err.message, setError);
-        },
-        {
-          enableHighAccuracy: true, // 정확도 우선 모드
-          timeout: 60000, // 1분 이내에 응답 없으면 에러 발생
-          maximumAge: 0 // 항상 최신 위치 정보 수집
+    const checkGeolocation = async () => {
+      if ('permissions' in navigator) {
+        //현 브라우저가 Geolocation API를 지원하는지 확인
+        try {
+          const result = await navigator.permissions.query({ name: 'geolocation' }); //사용자의 위치정보에 대한 권한을 요청
+          if (result.state === 'granted' || result.state === 'prompt') {
+            //허용했을때, 처음위치권한요청시
+            navigator.geolocation.getCurrentPosition(
+              //사용자의 현재 위치를 요청
+              async (position) => {
+                const { latitude, longitude } = position.coords;
+                setLocation({ lat: latitude, lng: longitude });
+                await getAddress(latitude, longitude);
+              },
+              (err) => {
+                showErrorMsg(err.message, setError);
+                alert('위치 권한이 거부되었습니다. 허용하지 않으면 스탬프를 찍을 수 없습니다.');
+              },
+              {
+                enableHighAccuracy: true, // 정확도 우선 모드
+                timeout: 60000, // 1분 이내에 응답 없으면 에러 발생
+                maximumAge: 0 // 항상 최신 위치 정보 수집
+              }
+            );
+          } else if (result.state === 'denied') {
+            // 위치 권한이 거부된 경우
+            alert('위치 권한이 거부되었습니다. 허용하지 않으면 스탬프를 찍을 수 없습니다.');
+            showErrorMsg('위치 권한이 거부되었습니다.', setError);
+          }
+        } catch (error) {
+          console.log('권한을 확인하는 중 오류가 발생했습니다.', error);
         }
-      );
-    } else {
-      console.log('오류가 발생했습니다.');
-    }
+      } else {
+        console.log('Geolocation API를 지원하지 않는 브라우저입니다.');
+      }
+    };
+    checkGeolocation();
   }, []);
 
   return {
