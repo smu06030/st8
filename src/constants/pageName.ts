951aa6168e2e5a77842a3ff2b15bcc2604f7a666
lim-bora <lbrkr96@naver.com>
2024-11-13 20:40:19 +0900

    [#125]add:모바일랜딩페이지 네비히든, pc지도페이지에서 푸터히든

--- src/constants/pageName.ts
+++ src/constants/pageName.ts
@@ -12,5 +12,6 @@ export const PAGE_NAMES = {
   BOOKMARK: { page: '찜한여행지', link: '/book-mark' },
   RESET_PASSWORD: { page: '비밀번호 찾기', link: '/reset-password' },
   RESET_SUCCESS: { page: '비밀번호 찾기', link: '/reset-success' },
-  UPDATE_SUCCESS: { page: '비밀번호 변경', link: '/update-success' }
+  UPDATE_SUCCESS: { page: '비밀번호 변경', link: '/update-success' },
+  LANDING: { page: '랜딩', link: '/landing' }
 };
