dfbc593fb5a9ceba7dd3cdd403248318e00d6772
incheol <smu06030>
2024-11-13 12:52:17 +0900

    [#111] style: 메인 추천 여행지 섹션 pagination 추가

--- src/components/mainPage/swiper/MainTourismSwiper.tsx
+++ src/components/mainPage/swiper/MainTourismSwiper.tsx
@@ -1,12 +1,16 @@
 'use client';
 
-import { Pagination } from 'swiper/modules';
+import { useState } from 'react';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Tourism } from '@/types/tourism/tourism.type';
 import MainTourismCard from './MainTourismCard';
+import useMediaSize from '@/hooks/useMediaSize';
 
+import '@/styles/mainTourismSwiper.css';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 interface MainTourismSwiperPropsType {
   tourismList: Tourism[];
@@ -14,42 +18,93 @@ interface MainTourismSwiperPropsType {
 }
 
 const MainTourismSwiper = ({ tourismList, userId }: MainTourismSwiperPropsType) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const isDesktop = useMediaSize();
+
+  // pagination 보이게 하기
+
+  console.log(activeIndex);
   return (
-    <Swiper
-      slidesPerView={1.1}
-      spaceBetween={10}
-      centeredSlides={false}
-      grabCursor={true}
-      modules={[Pagination]}
-      breakpoints={{
-        480: {
-          slidesPerView: 2.1
-        },
-        640: {
-          slidesPerView: 3.1
-        },
-        1024: {
-          slidesPerView: 3.7
-        }
-      }}
-      className="mt-5"
-    >
-      {tourismList
-        .filter((_, index) => index < 6)
-        .map((tourism) => (
-          <SwiperSlide key={tourism.contentid}>
-            <MainTourismCard
-              key={tourism.contentid}
-              userId={userId}
-              contentId={tourism.contentid || '등록되지 않는 여행지'}
-              title={tourism.title || '등록되지 않는 여행지'}
-              firstimage={tourism.firstimage}
-              description={tourism.supabaseText || '여행지 정보 없음'}
-              isBookmarked={tourism.isBookmarked}
-            />
-          </SwiperSlide>
-        ))}
-    </Swiper>
+    <>
+      {isDesktop ? (
+        <Swiper
+          key="desktop-swiper"
+          slidesPerView={3.7}
+          spaceBetween={10}
+          centeredSlides={true}
+          initialSlide={2}
+          grabCursor={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false
+          }}
+          pagination={{
+            clickable: true,
+            dynamicBullets: true
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation, Autoplay]}
+          onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+          className="mt-5 lg:mt-[26px]"
+          style={{ overflow: 'visible', overflowX: 'clip' }}
+        >
+          {tourismList
+            .filter((_, index) => index < 6)
+            .map((tourism, index) => (
+              <SwiperSlide key={tourism.contentid} className="swiper-slide">
+                <MainTourismCard
+                  key={tourism.contentid}
+                  userId={userId}
+                  contentId={tourism.contentid || '등록되지 않는 여행지'}
+                  title={tourism.title || '등록되지 않는 여행지'}
+                  firstimage={tourism.firstimage}
+                  description={tourism.supabaseText || '여행지 정보 없음'}
+                  isBookmarked={tourism.isBookmarked}
+                  activeIndex={activeIndex}
+                  index={index}
+                />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      ) : (
+        <Swiper
+          key="mobile-swiper"
+          slidesPerView={1.1}
+          spaceBetween={10}
+          centeredSlides={false}
+          grabCursor={true}
+          modules={[Pagination]}
+          breakpoints={{
+            480: {
+              slidesPerView: 2.1
+            },
+            640: {
+              slidesPerView: 3.1
+            },
+            1024: {
+              slidesPerView: 3.7
+            }
+          }}
+          className="mt-5"
+        >
+          {tourismList
+            .filter((_, index) => index < 6)
+            .map((tourism) => (
+              <SwiperSlide key={tourism.contentid} className="swiper-slide">
+                <MainTourismCard
+                  key={tourism.contentid}
+                  userId={userId}
+                  contentId={tourism.contentid || '등록되지 않는 여행지'}
+                  title={tourism.title || '등록되지 않는 여행지'}
+                  firstimage={tourism.firstimage}
+                  description={tourism.supabaseText || '여행지 정보 없음'}
+                  isBookmarked={tourism.isBookmarked}
+                />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      )}
+    </>
   );
 };
 
