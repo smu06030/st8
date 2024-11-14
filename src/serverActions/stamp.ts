ce69922737a88dd42990c7c83e75c04e89296343
incheol <smu06030>
2024-11-14 16:15:18 +0900

    [#124] feat: 에러, not-found 페이지 생성

--- src/serverActions/stamp.ts
+++ src/serverActions/stamp.ts
@@ -4,17 +4,21 @@ import { createClient } from '@/utils/supabase/server';
 import { Stamp } from '@/types/supabase/table.type';
 
 // 로그인유저의 스템프 항목 전부 + 스탬프 활성화된 데이터만
-export const getStampList = async (userId: string): Promise<Stamp[]> => {
+export const getStampList = async (userId: string): Promise<Stamp[] | Response> => {
   const serverClient = createClient();
 
   const { data, error } = await serverClient.from('stamp').select('*').eq('user_id', userId).eq('visited', true);
 
-  if (error) {
-    console.error('스탬프 정보 가져오기 오류 :', error.message);
-    throw new Error('스탬프 데이터를 가져오는 중 오류가 발생했습니다.' + error.message);
-  }
+  return Response.json({
+    message: '스탬프 데이터를 가져오는 중 오류가 발생했습니다.',
+    error: error
+  });
 
-  return data;
+  // if (error) {
+  //   console.error('스탬프 정보 가져오기 오류 :', error.message);
+  // }
+
+  // return data;
 };
 
 // 로그인유저가 찍은 스탬프의 별칭만 업데이트
