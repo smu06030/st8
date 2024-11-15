47ebe72aa85c5b7a731eb34c50fe7d698462860c
kyu <kyuli94p@gmail.com>
2024-11-15 12:06:28 +0900

    update:리다이렉트  배포url추가

--- src/components/auth/reset/ResetPasswordForm.tsx
+++ src/components/auth/reset/ResetPasswordForm.tsx
@@ -23,7 +23,12 @@ const ResetPasswordForm = () => {
     if (process.env.NODE_ENV === 'development') {
       return process.env.NEXT_PUBLIC_REDIRECT_URL_LOCAL || 'http://localhost:3000';
     }
-    return process.env.NEXT_PUBLIC_REDIRECT_URL_PRODUCTION || 'https://st8-dev.vercel.app/';
+
+    return (
+      process.env.NEXT_PUBLIC_REDIRECT_URL_PRODUCTION ||
+      process.env.NEXT_PUBLIC_REDIRECT_URL_BETA ||
+      'https://stamp-moa.vercel.app/'
+    );
   }, []);
 
   const onSubmit = async (profile: FormValues) => {
