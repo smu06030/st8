840e68d4ffd86dd11ae14144769bdfdbd654824b
incheol <smu06030>
2024-11-20 17:20:01 +0900

    [#193] feat: pwa 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="ko">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover"
+        />
+
+        <link rel="icon" type="image/png" href="/app-icon.png" />
+      </head>
       <body
         className={`bg-no-repeat font-regular lg:bg-[#F5F5F7] mo-only:bg-backgroundGradient ${geistSans.variable} ${geistMono.variable} ${!showSplash ? 'body-padding' : ''} antialiased`}
       >
